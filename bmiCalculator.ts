122c5aa4405028996d5dff2cd0a22278460ac59e
gmolin <gmolin@student.hive.fi>
2020-04-13 18:20:56 +0300

    ex 9.3 completed

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,5 +1,24 @@
 type Check = string;
 
+interface Value {
+	value1: number;
+	value2: number;
+}
+
+const parseInput = (argc: Array<string>) : Value => {
+	if (argc.length != 4)
+		throw new Error ("Usage: npm run calculateBmi <weight> <height>");
+	if (!isNaN(Number(argc[2])) && !isNaN(Number(argc[3])))
+	{
+		return {
+			value1: parseInt(process.argv[2], 10),
+			value2: parseInt(process.argv[3], 10)
+		}
+	}
+	else
+		throw new Error ("weight and height need to be a number");
+}
+
 const bmiCalculator = (weight: number, height: number) : Check =>{
 	let bmi: number;
 	height /= 100;
@@ -15,7 +34,8 @@ const bmiCalculator = (weight: number, height: number) : Check =>{
 }
 
 try{
-	console.log(bmiCalculator(0, 0));
+	const {value1, value2} = parseInput(process.argv);
+	console.log(bmiCalculator(value1, value2));
 }
 catch (e){
 	console.log("Error:", e.message);
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -8,7 +8,19 @@ interface Exercise{
 	average: number;
 }
 
-let array = [1, 3, 3, 0, 3, 3, 1, 1];
+let array = [3, 0, 2, 4.5, 0, 3, 1];
+const parseInput = (argc: Array<string>) : Array<number> => {
+	let array2 = [];
+	if (argc.length < 4)
+		throw new Error("Usage: npm run calculateExercise <target value> <daily exercises e.g. '0 1 3' = 3 days: d0:0h, d1:1h, d2:3h")
+	for (let i = 2; i < argc.length; i++)
+	{
+		if (isNaN(Number(argc[i])))
+			throw new Error ("value is not a number")
+		array2.push(parseInt(argc[i], 10));
+	}
+	return array2;
+}
 
 const calculateExercises = (data: Array<number>, target: number) : Exercise =>{
 	let count = 0;
@@ -20,7 +32,7 @@ const calculateExercises = (data: Array<number>, target: number) : Exercise =>{
 	for (var i = 0; i < data.length; i++)
 		(array[i] > 0) ? count++ : 0;
 	for (var i = 0; i < data.length; i++)
-		sum += array[i];
+		sum += data[i];
 	if (count >= target && (sum / data.length) < 1.5){
 		rating = 2;
 		des = "Good job, you met your target!"
@@ -45,7 +57,11 @@ const calculateExercises = (data: Array<number>, target: number) : Exercise =>{
 }
 
 try{
-	console.log(calculateExercises(array, 4));
+	const array2: Array<number> =  parseInput(process.argv);
+	let target: number;
+	target = array2[0];
+	array2.splice(0, 1);
+	console.log(calculateExercises(array2, target));
 }
 catch(e){
 	console.log("Error:", e.message);
